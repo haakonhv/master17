bddbcc30df5e366c79070a6b18eaecbb314305a6
hoeiland <haakoho@stud.ntnu.no>
2017-02-28 15:49:28 +0100

    bygd litt på ny state

--- src/markov2/Builder.java
+++ src/markov2/Builder.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import master17.DatabaseHandler;
+
 import master17.StateBuilder;
 
 public class Builder {
@@ -28,10 +29,13 @@ public class Builder {
 			float xEnd = eventSet.getFloat("Xend");
 			float yEnd = eventSet.getFloat("Yend");
 			int startZone = StateBuilder.getZoneFromCoordinates(xStart, yStart);
+			int endZone = StateBuilder.getZoneFromCoordinates(xEnd, yEnd);
 			int statePeriod = StateBuilder.getPeriod(minute, period);
 			int matchStatus = StateBuilder.getMatchStatus(goalDifference, homeID, teamID);
 			int reward = StateBuilder.getReward(action, teamID==homeID);
 			String team = "";
+			State startState;
+			State endState;
 
 			if (teamID == homeID){
 				team = "Home";
@@ -46,11 +50,51 @@ public class Builder {
 
 			if (!nextAction.equals("Aerial duel")){
 				if (action.equals("Pass") || action.equals("Long pass") || action.equals("Cross") || action.equals("Free kick pass") || action.equals("Throw in taken") || action.equals("Corner taken")){
-					if (outcome == 1){
-						if (stateList.size() == 0){
-							State startState = new State(stateID, startZone, team, statePeriod, matchStatus, reward);;
+					if (outcome == 1){ // hvis outcome er 1 -> start og end state har samme lag
+						//lager start state
+						if (stateList.size() == 0){ //hvis statelist tom -> legg til ny state
+							startState = new State(stateID, startZone, team, statePeriod, matchStatus, reward);
+							stateList.add(startState);
+							stateID++;
 						}
-
+						else { //hvis statelist ikke tom
+							boolean startStateExists = false;
+							for (int i = 0; i < stateList.size(); i++){
+								State s = stateList.get(i);
+								if(s.getZone() == startZone && s.getPeriod() == statePeriod //hvis state finnes fra før
+										&& s.getTeam().equals(team) && s.getMatchStatus() == matchStatus
+										&& s.getReward() == reward){
+									s.incrementOccurrence();
+									startState = s;
+									startStateExists = true;
+								}
+								if (!startStateExists){
+									startState = new State(stateID, startZone, team, statePeriod, matchStatus, reward);
+									stateList.add(startState);
+									stateID++;
+								}
+							}
+						}
+						//lager end state
+						boolean endStateExists = false;
+						for (int i = 0; i < stateList.size(); i++){
+							State s = stateList.get(i);
+							if(s.getZone() == endZone && s.getPeriod() == statePeriod //hvis state finnes fra før
+									&& s.getTeam().equals(team) && s.getMatchStatus() == matchStatus
+									&& s.getReward() == reward){
+								s.incrementOccurrence();
+								endState = s;
+								endStateExists = true;
+							}
+						}
+						if (!endStateExists){
+							endState = new State(stateID, endZone, team, statePeriod, matchStatus, reward);
+							stateList.add(endState);
+							stateID++;
+						}
+					}
+					else {//outcome ==0 -> start state hos team som slo pasning, end state har ikke noe team
+						
 					}
 				}
 			}
